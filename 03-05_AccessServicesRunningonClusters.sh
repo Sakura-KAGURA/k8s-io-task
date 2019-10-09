ab34e26cd355b1b25bae72c682495db121c47b8d
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-09 22:15:19 +0900

    Create 03-05_AccessServicesRunningonClusters.sh

---
+++ 03-05_AccessServicesRunningonClusters.sh
@@ -0,0 +1,14 @@
+
+# Accessing services running on the cluster
+## Ways to connect アクセス方法
+1. 
+1. 
+## ビルトインサービスの発見
+  - kubectl cluster-info
+## manually constructing apiserver proxy URLs
+  - 
+## Using web browsers to access services running on the cluster
+
+
+
+
