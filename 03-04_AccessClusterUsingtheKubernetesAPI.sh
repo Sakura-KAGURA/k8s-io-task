2779352463152b914ef46aab8d84abdb69ac3cae
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-09 19:43:55 +0900

    Update 03-04_AccessClusterUsingtheKubernetesAPI.sh

--- 03-04_AccessClusterUsingtheKubernetesAPI.sh
+++ 03-04_AccessClusterUsingtheKubernetesAPI.sh
@@ -1,6 +1,6 @@
 
-### curl -sf https://raw.githubusercontent.com/Sakura-KAGURA/ubt_setup_script/master/install_ansible_ubt16.sh | sh -s
-# curl -sf  https://raw.githubusercontent.com/Sakura-KAGURA/k8s-io-task/master/03-04_AccessClusterUsingtheKubernetesAPI.sh | sh -s
+### (sample) curl -sf https://raw.githubusercontent.com/Sakura-KAGURA/ubt_setup_script/master/install_ansible_ubt16.sh | sh -s
+# usage: curl -sf  https://raw.githubusercontent.com/Sakura-KAGURA/k8s-io-task/master/03-04_AccessClusterUsingtheKubernetesAPI.sh | sh -s
 
 # Before you begin
 ## kubectl 
